cbd35aac33fcc33fa47eaea40c5396f2af809cb5
Dana <danayim@bu.edu>
2026-02-22 19:48:18 +0900

    feat: Hero component is created & imported to page.tsx.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+// page.tsx = unique content for this specific route (the home page).
+// This file is injected into the { children } slot of layout.tsx file.
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main className="max-width padding-x padding-y">
+    <main className="overflow-hidden">
+      <Hero />
     </main>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,34 @@
+// Hero.tsx = main component 
+"use client";
+import Image from "next/image";
+
+const Hero = () => {
+    return (
+        <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-width">
+            <div className="pt-36 padding-x flex-1">
+                <h1 className="text-primary-pink text-[50px] sm:text-[64px] font-bold font-libre leading-[1.1]">
+                    Uncover your next favorite story.
+                </h1>
+
+                <p className="text-[20px] text-primary-pink font-sans mt-5">
+                    The modern sanctuary for readers to track, discover, and organize their literary world.
+                </p>
+
+                <button className="custom-btn mt-10 min-w-[170px]">
+                    Explore Books
+                </button>
+            </div>
+
+            <div className="xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen">
+                <Image 
+                    src="/hero.png"
+                    width={450}
+                    height={450}
+                    alt="hero image"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
