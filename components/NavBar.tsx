c5a3cd5a076d72da567a5d9349b2526ce8d2bdb8
Dana <danayim@bu.edu>
2026-02-22 18:30:50 +0900

    documentation: minor UI color updates.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     return (
         <header className="w-full absolute z-10">
             <nav className="max-width flex-between padding-x padding-y bg-transparent">
-                <Link href="/" className="text-2xl font-bold text-primary-plum font-libre">
+                <Link href="/" className="text-2xl font-bold text-primary-pink font-libre">
                     BookHub
                 </Link>
 
