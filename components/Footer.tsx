fcf8eee3d92c6c13d5632e8d6e12c055ec810040
Dana <danayim@bu.edu>
2026-02-28 00:01:07 +0900

    feat: add about page.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@
 // provides brand name and copyright info.
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const Footer = () => {
@@ -11,13 +12,23 @@ const Footer = () => {
     const borderColor = isJournal ? "border-primary-white" : "border-primary-pink";
 
     return (
-        <footer className={`flex flex-col mt-5 border-t ${borderColor}`}>
-            <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <footer className={`flex flex-col w-full mt-5 border-t ${borderColor}`}>
+            <div className="flex w-full max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+                {/* Left */}
                 <div className="flex flex-col justify-start items-start gap-6">
                     <p className={`text-base ${textColor} font-karrik`}>
                         mimi 2026 <br />
                         All rights reserved &copy;
                     </p>
+                </div>
+                {/* Right */}
+                <div className="flex items-center">
+                    <Link
+                        href="/about"
+                        className={`text-base ${textColor} font-karrik`}
+                    >
+                        About
+                    </Link>
                 </div>
             </div>
         </footer>
